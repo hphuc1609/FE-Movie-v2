0873a478f0a9c44b1770ae0993c051064fd796ee
Phúc Lưu <luuboy91@gmail.com>
2024-06-26 23:12:47 +0700

    feat: Add play button component UI

---
+++ src/components/common/play-button.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/lib/utils'
+import { Play } from 'lucide-react'
+import React from 'react'
+
+interface PlayButtonProps {
+  onClick?: () => void
+  className?: string
+  PlayIconProps?: React.SVGProps<SVGSVGElement>
+}
+
+export default function PlayButton({ onClick, className, PlayIconProps }: PlayButtonProps) {
+  return (
+    <div
+      className={cn(
+        'w-[72px] h-[72px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center rounded-full bg-primary-color cursor-pointer',
+        className,
+      )}
+      onClick={onClick}
+    >
+      <Play
+        size={30}
+        fill='white'
+        {...PlayIconProps}
+      />
+    </div>
+  )
+}
