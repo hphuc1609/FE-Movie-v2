3c629ad9534e3562030217e45ea5f62b090314e9
Phúc Lưu <luuboy91@gmail.com>
2024-07-01 19:20:00 +0700

    hotfix: Render message not found issue

--- src/components/common/table-pagination/index.tsx
+++ src/components/common/table-pagination/index.tsx
@@ -1,4 +1,4 @@
-import { MovieCategoryParams } from '@/models/list-movie'
+import { MovieCategoryResponse } from '@/models/list-movie'
 import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
@@ -16,8 +16,8 @@ export default function TablePagination(props: TablePaginationProps) {
   const { category, data } = props
   const searchParams = useSearchParams()
   const currentPage = JSON.parse(searchParams.get('page') || '1')
-  const pagination = (data.params?.pagination ||
-    data.pagination) as MovieCategoryParams['pagination']
+  const pagination = data.params?.pagination || (data.pagination as MovieCategoryResponse['data'])
+  console.log('🚀 ~ TablePagination ~ pagination:', pagination)
 
   // Memoize the list of pages to prevent unnecessary re-renders
   const pagesToShow = useMemo(() => {
@@ -41,36 +41,38 @@ export default function TablePagination(props: TablePaginationProps) {
 
   return (
     <>
-      <div className='flex flex-col gap-10 pb-9'>
-        <div className='grid grid-cols-6 gap-5 max-sm:grid-cols-2 max-md:grid-cols-3 max-lg:grid-cols-4 overflow-hidden'>
-          <CardImage
-            data={data}
-            paramCategory={category}
-            itemLength={pagination?.totalItemsPerPage}
-          />
-        </div>
-        {pagesToShow?.length > 1 ? (
-          <PaginationCustom
-            pageToShow={pagesToShow}
-            category={category}
-            currentPage={currentPage}
-            pagination={pagination}
-          />
-        ) : (
-          <div className='flex flex-col gap-1 items-center'>
-            <p className='text-center text-2xl font-medium'>
-              <span className='text-primary-color'>Không tìm thấy kết quả tìm kiếm: </span>
-              {props.keyword}
-            </p>
-            <Link
-              href={'/'}
-              className='text-center text-base flex gap-1 items-center text-primary-color'
-            >
-              <ArrowLeft size={18} /> Quay lại trang chủ
-            </Link>
+      {data.items?.length ? (
+        <div className='flex flex-col gap-10 pb-9'>
+          <div className='grid grid-cols-6 gap-5 max-sm:grid-cols-2 max-md:grid-cols-3 max-lg:grid-cols-4 overflow-hidden'>
+            <CardImage
+              data={data}
+              paramCategory={category}
+              itemLength={pagination?.totalItemsPerPage}
+            />
           </div>
-        )}
-      </div>
+          {pagesToShow?.length > 1 && (
+            <PaginationCustom
+              pageToShow={pagesToShow}
+              category={category}
+              currentPage={currentPage}
+              pagination={pagination}
+            />
+          )}
+        </div>
+      ) : (
+        <div className='flex flex-col gap-1 items-center'>
+          <p className='text-center text-2xl font-medium'>
+            <span className='text-primary-color'>Không tìm thấy kết quả tìm kiếm: </span>
+            {props.keyword}
+          </p>
+          <Link
+            href={'/'}
+            className='text-center text-base flex gap-1 items-center text-primary-color'
+          >
+            <ArrowLeft size={18} /> Quay lại trang chủ
+          </Link>
+        </div>
+      )}
     </>
   )
 }
