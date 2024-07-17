6da12e860945ab06caeea78d9d088f9fc127bcb2
Phúc Lưu <luuboy91@gmail.com>
2024-07-17 21:36:56 +0700

    feat: Add react query package and custom fetch data hook

---
+++ src/custom-hooks/useFetchData.ts
@@ -0,0 +1,19 @@
+import { QueryKey, useQuery, UseQueryOptions } from '@tanstack/react-query'
+
+interface FetchDataOptions<T>
+  extends Omit<UseQueryOptions<T, Error, T, QueryKey>, 'queryKey' | 'queryFn'> {
+  queryKey: QueryKey
+  queryFn: () => Promise<T>
+}
+
+const useFetchData = <T>({ queryKey, queryFn, ...options }: FetchDataOptions<T>) => {
+  return useQuery({
+    queryKey: [queryKey],
+    queryFn,
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    ...options,
+  })
+}
+
+export default useFetchData
