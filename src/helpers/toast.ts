389aee51680bee3ce5401f26d93b17352253640f
LT. Hoang Phuc <luuboy91@gmail.com>
2024-09-21 21:00:30 +0700

    chore: Update toast styles

--- src/helpers/toast.ts
+++ src/helpers/toast.ts
@@ -8,13 +8,14 @@ export const showToast = ({
 }: {
   description: string
   title?: string
-  variant?: 'success' | 'error' | 'info' | 'warning'
+  variant?: 'success' | 'error' | 'info' | 'warning' | 'default'
 }) => {
   const toastStyles = {
-    success: 'bg-green-500 border-none text-white',
-    error: 'bg-red-500 border-none text-white',
-    info: 'bg-blue-500 border-none text-white',
-    warning: 'bg-yellow-500 border-none text-white',
+    default: 'bg-neutral-700/80 border-none text-white',
+    success: 'bg-green-500/80 border-none text-white',
+    error: 'bg-red-500/80 border-none text-white',
+    info: 'bg-blue-500/80 border-none text-white',
+    warning: 'bg-yellow-500/80 border-none text-white',
   }
 
   toast({
