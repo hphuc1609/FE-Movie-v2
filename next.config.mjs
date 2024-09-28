f8740d5f6ff64d2c4c6715f27869f4d2a4f08b84
LT. Hoang Phuc <luuboy91@gmail.com>
2024-09-28 20:33:44 +0700

    chore: Unoptimize image

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-    // unoptimized: process.env.NODE_ENV === 'production' ? true : false,
+    unoptimized: true,
   },
   async headers() {
     return [
